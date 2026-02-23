31d6b876eee8a150cb2ae02bdfcccf23697f972a
Limolix <andrejnagirnak395@gmail.com>
2026-02-24 01:56:48 +0800

    add file tests

---
+++ TestProject2/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using task2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject2.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
