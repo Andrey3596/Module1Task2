e2303fa9fe9765a66788130bca6ccf951980006b
Limolix <andrejnagirnak395@gmail.com>
2026-02-24 01:17:11 +0800

    add for

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,13 @@
 ﻿Console.WriteLine("Введите число");
 var n = int.Parse(Console.ReadLine());
+
+
+for (int i = 1; i < n; i++)
+{
+    if (i * i > n)
+    {
+        Console.WriteLine(n);
+        break;
+    }
+    Console.WriteLine(i * i);
+}
