084aa638032a586c15096124e50392db646b1cc7
Limolix <andrejnagirnak395@gmail.com>
2026-02-24 11:04:10 +0800

    add some test

--- TestProject2/UnitTest1.cs
+++ TestProject2/UnitTest1.cs
@@ -20,5 +20,53 @@ namespace TestProject2.Tests
             string answer = Logic.FindFirstGreaterThanN(n);
             Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual("1 0", answer);
         }
+
+
+
+        [TestMethod()]
+        public void Test1()
+        {
+
+            int n = 0;
+            string answer = Logic.FindFirstGreaterThanN(n);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual("1 0", answer);
+        }
+
+
+
+
+
+        [TestMethod()]
+        public void Test2()
+        {
+
+            int n = 4;
+            string answer = Logic.FindFirstGreaterThanN(n);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual("1 4 4", answer);
+        }
+
+
+
+
+        [TestMethod()]
+        public void Test3()
+        {
+
+            int n = 5;
+            string answer = Logic.FindFirstGreaterThanN(n);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual("1 4 9 5", answer);
+        }
+
+
+
+
+        [TestMethod()]
+        public void Test4()
+        {
+
+            int n = 5;
+            string answer = Logic.FindFirstGreaterThanN(n);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual("1 4 9 5", answer);
+        }
     }
 }
